0067e9cbcdb71f2cb12c20c6620296430a7dc623
Nicolas Rodovalho <nicolasrlima@gmail.com>
2020-04-28 11:06:36 -0300

    minor syntax changes

--- script.js
+++ script.js
@@ -5,23 +5,20 @@ const resultSpan = document.querySelector('span');
 function isPalindrome(number) {
   const numberString = number.toString();
   const numberLength = numberString.length;
-  let i;
 
-  if (numberLength === 1) {
-    return true;
-  }
-  for (i = 0; i < Math.ceil(numberLength / 2); i += 1) {
+  if (numberLength === 1) return true;
+
+  for (let i = 0; i < Math.ceil(numberLength / 2); i += 1) {
     if (numberString[i] !== numberString[numberLength - i - 1]) return false;
   }
   return true;
 }
 
 function findNearestPalindrome(number) {
-  let i;
   const upperPalindrome = { value: 0, d: 0 };
   const lowerPalindrome = { value: 0, d: 0 };
 
-  for (i = number; i <= 9999999; i += 1) {
+  for (let i = number; i <= 9999999; i += 1) {
     if (isPalindrome(i) === true) {
       upperPalindrome.value = i;
       upperPalindrome.d = i - number;
@@ -29,14 +26,16 @@ function findNearestPalindrome(number) {
     }
   }
 
-  for (i = number; i >= 0; i -= 1) {
+  for (let i = number; i >= 0; i -= 1) {
     if (isPalindrome(i) === true) {
       lowerPalindrome.value = i;
       lowerPalindrome.d = number - i;
       break;
     }
   }
+
   if (upperPalindrome.d <= lowerPalindrome.d) return upperPalindrome.value;
+
   return lowerPalindrome.value;
 }
 
